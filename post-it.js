04cc20a6f2bc12cdff12a78d7c04087be12cfa24
Apprentice <apprentice@dbc-24.local>
2013-11-05 23:36:18 -0800

    post it v2

--- post-it.js
+++ post-it.js
@@ -1,21 +1,32 @@
 var postNumber = 0
+var boardNum = 0
 
-var Board = function( selector ) {
-  var $elem = $( selector );
+var Board = function(selector, boardNum) {
+  var $board = $(selector);
 
   function initialize() {
-    $elem.on("click", newPostIt);
+    $board.on("click", newPostIt);
+    $board.addClass("hidden");
+    $("#board-"+boardNum).removeClass("hidden");
+  };
+
+  function newPostIt() {
+    var x = event.offsetX;
+    var y = event.offsetY;
+    new PostIt(x, y, boardNum);
+    postNumber += 1;
   };
 
   initialize();
 };
 
-var PostIt = function() {
+var PostIt = function(x, y, boardNum) {
 
   function initialize() {
-    $("#board").append('<div class="post-it" id="'+postNumber+'"><div class="header"><a href="#">X</a></div><div contenteditable="true" class="content"></div></div>')
-    $("#"+postNumber).css({'top': event.pageY, 'left': event.pageX});
+    $(".board#board-"+boardNum).append('<div class="post-it board-'+boardNum+' post-'+postNumber+'"><div class="header"><a href="#">X</a></div><div contenteditable="true" class="content"></div></div>')
+    $(".post-"+postNumber).css({'top': y, 'left': x});
     $(".post-it").draggable({handle: ".header"});
+
     $(".content").on("click", stopPostItCreation);
     $("a").on("click", deletePostIt);
   };
@@ -26,18 +37,37 @@ var PostIt = function() {
 
   function deletePostIt(e){
     e.stopPropagation();
-    var $parent = $(this.parentElement.parentElement);
-    $parent.remove();
+    var $thisPostIt = $(this.parentElement.parentElement);
+    $thisPostIt.remove();
   };
 
   initialize();
 };
 
-$(function() {
-  new Board('#board');
-});
+var NavBar = function() {
 
-function newPostIt() {
-  new PostIt;
-  postNumber += 1;
-};
+  function handler() {
+    $("#board-selector").on("click", "#new-board", addNewBoard);
+    $("#board-selector").on("click", "#board-list li", showBoard);
+  };
+
+  function addNewBoard() {
+    boardNum += 1;
+    $("#board-area").append("<div class='hidden board' id='board-"+boardNum+"'></div>");
+    $("#board-list").append("<li class='board-"+boardNum+"'>board "+boardNum+"</li>");
+
+    new Board('.board', boardNum);
+  };
+
+  function showBoard() {
+    var thisIdName = this.className;
+    $(".board").addClass("hidden");
+    $("#"+thisIdName).removeClass("hidden");
+  };
+
+  handler();
+}
+
+$(function() {
+  new NavBar();
+});
